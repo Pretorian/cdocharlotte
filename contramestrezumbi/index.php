591a7bfd3270e91db59436926bf4469de6209ea5
mestreftp <mestreftp@p3nlhftpg172.shr.prod.phx3.secureserver.net>
2015-08-06 22:03:34 -0700

    zumbi's profile fix title

---
+++ contramestrezumbi/index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<!-- master default -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<meta name="revisit-after" content="15 days">
+<meta name="distribution" content="web">
+<meta http-equiv="refresh" content="21600">
+<meta name="google-site-verification" content="xyhIqwkqgdpdDmVPJt7oIrKeIBYEOGcPuCNJXDjVrGg" />
+<meta name="keywords" content="Capoeira, CDO, Cordao de Ouro, Charlotte, Mestre Esquilo, Mestre Saussana, BJJ, Brasil, Just, Brazil, Brazilian, Rhythms ">
+<meta name="description" content="2013 2nd International Capoeira Festival - CDO Charlotte. The event is being held August 8th – 10th at the University of North Carolina Charlotte’s campus. The schedule is listed on the Facebook event page and at www.charlottecapoeira.com. This exciting event showcases the talent, hard-work, and dedication of the Brazilian Art Center students and brings together many capoeira masters, teachers, and students from all over the world.">
+
+<!-- facebook meta -->
+<meta property="og:site_name" content="Cordao de Ouro Charlotte - Contra Mestre Zumbi" />
+<meta property="og:title" content="Charlotte Capoeira - Contra Mestre Zumbi" />
+<meta property="og:description" content="<?php echo $googledescription;?>" />
+
+<link rel="stylesheet" type="text/css" href="../css/style.css"/>
+<link rel="stylesheet" type="text/css" href="../css/epicslider.css"/>
+<link rel="stylesheet" type="text/css" href="../css/udt_shortcodes.css"/>
+<link rel="stylesheet" type="text/css" href="../css/udt_media_queries.css"/>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,800' rel='stylesheet' type='text/css'/>
+<link href='http://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'/>
+<title>Contra Mestre Zumbi - CDO Charlotte</title>
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js"></script>
+</head>
+
+<body>
+<!--start wrapper-->
+<!-- AddThis Smart Layers BEGIN -->
+<!-- Go to http://www.addthis.com/get/smart-layers to customize -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid="></script>
+<script type="text/javascript">
+  addthis.layers({
+    'theme' : 'transparent',
+    'share' : {
+      'position' : 'left',
+      'numPreferredServices' : 5
+    }, 
+    'follow' : {
+      'services' : [
+        {'service': 'facebook', 'id': 'charlottecapoeira'},
+        {'service': 'twitter', 'id': 'mestreesquilo'}
+      ]
+    },  
+    'whatsnext' : {},  
+    'recommended' : {
+      'title': 'Recommended for you:'
+    } 
+  });
+</script>
+<!-- AddThis Smart Layers END -->
+
+<div id="wrapper">
+
+   <!--start header-->
+    <div class="header">
+      <div class="inner">
+            <div class="logo-container">
+                 <div class="logo">
+                     <a class="navigateTo" href="/#homepage"><img src="/images/logo.menu.fw.png" title="charlottecapoeira" alt=""/></a>                    
+                 </div>
+           </div>
+            
+         <!--mobileMenu toggle-->
+         <div class="mobileMenuToggle"><a href=""></a></div>
+            
+          <!--navigation-->
+         <ul class="navigation">
+            <li><a class="navigateTo" href="/#homepage">Home</a></li>
+            <li><a class="navigateTo" href="/#about">About</a></li>
+            <li><a class="navigateTo" href="/mestreesquilo">Meet mestre Esquilo</a></li>
+            <li><a class="navigateTo" href="/batizado">Batizado</a></li>
+            <li><a class="navigateTo" href="/#folio">Projects</a></li>
+            <li><a class="navigateTo" href="/#contact">Contact</a></li>
+         </ul>
+        </div>
+    </div>
+    <!--end header--> 
+    <!--start page -->
+     <div id="project-8" class="page odd">
+        
+        <!--start content-->
+        <div class="content">
+        
+          <!--section title-->
+            <div class="section-title ">
+              <h3 class="section">Contra Mestre <span>Zumbi</span></h3>
+<h6 class="section"><span>Aristides Valeriano</span></h6>
+              
+            </div>
+          <!--end section title-->
+        
+             <div id="ajaxpage">
+                <!--start project display-->
+                <div class="project-display fullwidth">
+                  <img src="/images/gallery/zumbihome.jpg"  width="930" height="auto" alt=""/>
+                </div>  
+                <h1>
+                  Contra Mestre Zumbi
+                </h1>
+<div class="tabs_content" data-label_ref="3">
+					<blockquote class="theme">
+						<p>Capoeira is like playing chess, but instead of moving chessmen you’re moving your body.</p>
+						<p class="blockquote_cite">Contramestre Zumbi</p>
+					</blockquote>
+				</div>		
+<h6>
+<span>
+<p>Zumbi (Aristides Valeriano) was born in Sete Lagoas in Minas Gerais, Brazil.</p>
+<p> At the age of 13, Zumbi started to practice Capoeira with Mestre Esquilo. When Esquilo joined the group Cordão de Ouro, Zumbi and other students followed. Zumbi has completely dedicated his life to Capoeira and is Contra Mestre today. </p>
+<p>With his unique way of teaching Capoeira, Zumbi creates a fun and creative class each time. He combines martial arts, expressive movements and gymnastics into an exciting and challenging practice. After teaching workshops and performing throughout Europe and the Middle East, including Latvia, Israel, Austria, Sweden and Germany, Zumbi has made the US his home, where he hopes to show others the beauty and diversity of Brazilian culture. He currently gives classes in Charlotte NC.</p>
+</span>
+</h6>
+            </div>
+
+       </div>
+        <!--end content-->
+     
+     </div>
+    <!--end page --> 
+    
+    <!--start footer-->
+    <div id="footer" class="even">
+        <div class="content">
+            <!--social networks-->
+            <ul class="socialSmall even">
+                <!--
+                <li><a class="pinterest" href=""></a></li>
+                <li><a class="forrst" href=""></a></li>
+                <li><a class="googleplus" href=""></a></li>
+                <li><a class="myspace" href=""></a></li>
+                <li><a class="dribbble" href=""></a></li>
+                <li><a class="linkedin" href=""></a></li>
+              -->
+                <li><a target=_BLANK class="twitter" href="https://twitter.com/MestreEsquilo"></a></li>
+                <li><a target=_BLANK class="facebook" href="http://www.facebook.com/charlottecapoeira"></a></li>
+                <li><a target=_BLANK class="skype" href="skype:mestreesquilo?call"></a></li>
+                <li><a target=_BLANK class="flickr" href="http://www.flickr.com/photos/97198770@N05/"></a></li>
+                <li><a target=_BLANK class="myspace" href="http://www.youtube.com/channel/UCIoyYF2XEcns7wpExt4K1xg"></a></li>
+            </ul>
+           
+            <div class="logo">
+                          <a class="navigateTo" href="/"><img src="/images/logo-common.png" alt="#CDOCharlotte"></a>
+                        </div>
+                        <p class="copyright">&copy; Monobytes Web Works.2013. <br/>All rights reserved. Theme by <a href="http://monobytes.com" title="Monobytes Rules Your Web - MNBYTS">MNBYTS</a>.</p>
+            </div>
+
+        </div>
+    </div>
+    <!--end footer-->
+
+
+</div>        
+<!--end wrapper-->        
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-42710920-1', 'mestreesquilo.com');
+  ga('send', 'pageview');
+
+</script> 
+</body>
+</html>
