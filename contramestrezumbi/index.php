22e6a26a3f21dc077a0f29ac23953b3ac29a232a
OBAMA <gahiel@gmail.com>
2015-08-11 23:10:10 -0700

    fix merge

--- contramestrezumbi/index.php
+++ contramestrezumbi/index.php
@@ -11,7 +11,7 @@
 <meta http-equiv="refresh" content="21600">
 <meta name="google-site-verification" content="xyhIqwkqgdpdDmVPJt7oIrKeIBYEOGcPuCNJXDjVrGg" />
 <meta name="keywords" content="Capoeira, CDO, Cordao de Ouro, Charlotte, Mestre Esquilo, Mestre Saussana, BJJ, Brasil, Just, Brazil, Brazilian, Rhythms ">
-<meta name="description" content="2013 2nd International Capoeira Festival - CDO Charlotte. The event is being held August 8th – 10th at the University of North Carolina Charlotte’s campus. The schedule is listed on the Facebook event page and at www.charlottecapoeira.com. This exciting event showcases the talent, hard-work, and dedication of the Brazilian Art Center students and brings together many capoeira masters, teachers, and students from all over the world.">
+<meta name="description" content="Contra Mestre Zumbi, Capoeira is like playing chess, but instead of moving chessmen you’re moving your body.">
 
 <!-- facebook meta -->
 <meta property="og:site_name" content="Cordao de Ouro Charlotte - Contra Mestre Zumbi" />
